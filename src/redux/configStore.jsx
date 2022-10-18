ed2f423f2ed3c301f23a8ee803588fe0741b5d4e
DungTu <tutuyettdung@gmail.com>
2022-10-18 16:05:03 +0700

    first commit

---
+++ src/redux/configStore.jsx
@@ -0,0 +1,13 @@
+import { createStore, combineReducers } from "redux";
+
+
+
+const rootReducer = combineReducers({
+    //Nơi chứa các state của ứng dụng
+   
+  });
+  
+  export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
